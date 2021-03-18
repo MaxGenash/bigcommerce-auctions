53846e02a00e4b3c30bad7b2dea2c01173bf0ed8
MaxGenash <max.henash@bigcommerce.com>
2021-03-18 14:25:04 +0200

    add auctions pages

---
+++ pages/auctions/[slug].tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@components/common'
+
+export default function AuctionPage() {
+  return (
+    <h2>auction page</h2>
+  );
+}
+
+AuctionPage.Layout = Layout
---
+++ pages/auctions/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@components/common'
+
+export default function AuctionsList() {
+  return (
+    <h2>auctions list</h2>
+  );
+}
+
+AuctionsList.Layout = Layout
